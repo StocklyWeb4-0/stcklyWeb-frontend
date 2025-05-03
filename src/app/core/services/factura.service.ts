4a71f55c045c64eb8d659171afb1d80af48f40ec
Isabelcerons <isabelceron772@gmail.com>
2025-05-03 14:06:15 -0500

    modificacion archivo factura.service

--- src/app/core/services/factura.service.ts
+++ src/app/core/services/factura.service.ts
@@ -1,16 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FacturaService {
-  private apiUrl = `${environment.apiUrl}/facturas`;
-
+  // URL base hardcodeada para no depender de environment
+  private apiUrl = 'http://localhost:8080/facturas';
+  
   constructor(private http: HttpClient) {}
-
+  
   enviarFacturaPorCorreo(idVenta: number, email: string): Observable<any> {
     return this.http.post(`${this.apiUrl}/enviar-correo`, { idVenta, email });
   }
